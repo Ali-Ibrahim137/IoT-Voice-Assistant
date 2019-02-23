efd460d0ee13e8918bfb03c550acee5c35abaf3a
C137 <ali103575@gmail.com>
2019-02-23 15:59:06 +0200

    Devices page

--- Django_project/Vinus/urls.py
+++ Django_project/Vinus/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('devices/', views.devices, name='Vinus-devices-list'),
     path('',views.home, name='Vinus-home' ),
     path('about/',views.about, name='Vinus-about' ),
 ]
--- Django_project/Vinus/views.py
+++ Django_project/Vinus/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Device, THINGER_API, Resources
 
 def home(request):
     return render(request, 'Vinus/home.html')
 
 def about(request):
     return render(request, 'Vinus/about.html', {'title':'about'})
+
+def devices(request):
+    context = {
+        'object_list': Device.objects.filter(userr = request.user)
+    }
+    return render (request, 'Vinus/device_list.html', context)
